dc5ef2f9593f76070250db8185603901dce8d94e
Arif Bryan <arief372@gmail.com>
2022-06-21 16:30:43 +0700

    Add mv divider for current PID

--- outputControl.cpp
+++ outputControl.cpp
@@ -86,17 +86,17 @@ void OutputControl_TypeDef::Init() {
 	//ch1.pidV.SetConstants(1.5, 0.005, 0.2, LOOP_INTERVAL); // Trial & error
 	//ch1.pidV.SetConstants(1.8, 4.629, 0.054, LOOP_INTERVAL); // Ziegler Nichols
 	ch1.pidV.SetConstants(1.8, 30.0, 0.005, LOOP_INTERVAL); // Trial & Error
-	ch1.pidI.SetConstants(0.75, 25.0, 0.002, LOOP_INTERVAL);
+	ch1.pidI.SetConstants(1.2, 25.0, 0.002, LOOP_INTERVAL);
 	ch1.pidV.SetOutputRange(0, 0xFFFF);
 	ch1.pidI.SetOutputRange(0, 0xFFFF);
 	
 	ch2.pidV.SetConstants(1.8, 30.0, 0.005, LOOP_INTERVAL);
-	ch2.pidI.SetConstants(0.75, 25.0, 0.002, LOOP_INTERVAL);
+	ch2.pidI.SetConstants(1.2, 25.0, 0.002, LOOP_INTERVAL);
 	ch2.pidV.SetOutputRange(0, 0xFFFF);
 	ch2.pidI.SetOutputRange(0, 0xFFFF);
 	
 	ch3.pidV.SetConstants(1.8, 30.0, 0.005, LOOP_INTERVAL);
-	ch3.pidI.SetConstants(0.75, 25.0, 0.002, LOOP_INTERVAL);
+	ch3.pidI.SetConstants(1.2, 25.0, 0.002, LOOP_INTERVAL);
 	ch3.pidV.SetOutputRange(0, 0xFFFF);
 	ch3.pidI.SetOutputRange(0, 0xFFFF);
 		
@@ -241,7 +241,7 @@ void Channel_TypeDef::Handler() {
 		}
 		else {
 			// I = V / R
-			mv = pidI.GetOutput() / ina226->GetCurrentCal();
+			mv = pidI.GetOutput() / ina226->GetCurrentCal() / iSet;
 			pidV.Reset();
 			if (abs(pidI.GetError()) > 0.025) {
 				stableCounter = CH_STABLE_CNT;
